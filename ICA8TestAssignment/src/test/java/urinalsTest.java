d3fa2bbb8def47f46ad85094a3cdf210d609c4a2
vishwanathreddy9 <vyasa@asu.edu>
2022-10-27 19:39:06 -0700

    added fifth test case code

--- ICA8TestAssignment/src/test/java/urinalsTest.java
+++ ICA8TestAssignment/src/test/java/urinalsTest.java
@@ -5,7 +5,7 @@ public class urinalsTest {
     vacantUrinalsCount v = new vacantUrinalsCount();
     @Test
     void main() {
-        System.out.println("===Vishwanath Reddy Yasa====");
+        System.out.println("=== Vishwanath Reddy Yasa ====");
         System.out.println("====FIRST TEST EXECUTED====");
         Assertions.assertEquals(-1, v.vacantUrinals("011"));
         System.out.println("====SECOND TEST EXECUTED====");
@@ -14,5 +14,7 @@ public class urinalsTest {
         Assertions.assertEquals(2, v.vacantUrinals("0000"));
         System.out.println("====FOURTH TEST EXECUTED====");
         Assertions.assertEquals(1, v.vacantUrinals("10001"));
+        System.out.println("====FIFTH TEST EXECUTED====");
+        Assertions.assertEquals(0, v.vacantUrinals("1001"));
     }
 }

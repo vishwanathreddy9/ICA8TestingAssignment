3c1ba8f43569735a81eba0ad3ee6b7b5cd6efc5f
vishwanathreddy9 <vyasa@asu.edu>
2022-10-27 17:42:07 -0700

    unit test failing with initial case

--- ICA8TestAssignment/src/test/java/urinalsTest.java
+++ ICA8TestAssignment/src/test/java/urinalsTest.java
@@ -2,11 +2,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class urinalsTest {
-    vacantUrinalsCount v = new vacantUrinalsCount();
     @Test
     void main() {
         System.out.println("===Vishwanath Reddy Yasa====");
         System.out.println("====FIRST TEST EXECUTED====");
-        Assertions.assertEquals(false, v.goodString("vyasa") );
+        Assertions.assertEquals("vyasa", "vishwanathyasa" );
     }
 }

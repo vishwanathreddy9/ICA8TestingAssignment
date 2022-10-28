a4dd09bfe937c9299198c9480bb323bf08321451
vishwanathreddy9 <vyasa@asu.edu>
2022-10-27 17:44:11 -0700

    unit test passing for good string method

--- ICA8TestAssignment/src/test/java/urinalsTest.java
+++ ICA8TestAssignment/src/test/java/urinalsTest.java
@@ -2,10 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class urinalsTest {
+    vacantUrinalsCount v = new vacantUrinalsCount();
     @Test
     void main() {
         System.out.println("===Vishwanath Reddy Yasa====");
         System.out.println("====FIRST TEST EXECUTED====");
-        Assertions.assertEquals("vyasa", "vishwanathyasa" );
+        Assertions.assertEquals(true, v.goodString("vyasa") );
     }
 }

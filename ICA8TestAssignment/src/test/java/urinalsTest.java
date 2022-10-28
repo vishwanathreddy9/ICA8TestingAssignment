caec0a9d0789e7bf3ad54b9a72f63238f3333e91
vishwanathreddy9 <vyasa@asu.edu>
2022-10-27 17:38:18 -0700

    Commit with unit tests added

--- ICA8TestAssignment/src/test/java/urinalsTest.java
+++ ICA8TestAssignment/src/test/java/urinalsTest.java
@@ -2,6 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class urinalsTest {
+    vacantUrinalsCount v = new vacantUrinalsCount();
     @Test
-    void main() {}
+    void main() {
+        System.out.println("===Vishwanath Reddy Yasa====");
+        System.out.println("====FIRST TEST EXECUTED====");
+        Assertions.assertEquals(false, v.goodString("vyasa") );
+    }
 }

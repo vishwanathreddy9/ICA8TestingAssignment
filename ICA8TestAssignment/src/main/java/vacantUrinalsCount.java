ce11d024bc5c6fa9ff4ed9940b8e33d76d045471
vishwanathreddy9 <vyasa@asu.edu>
2022-10-27 19:19:54 -0700

    implemented code for case 2

--- ICA8TestAssignment/src/main/java/vacantUrinalsCount.java
+++ ICA8TestAssignment/src/main/java/vacantUrinalsCount.java
@@ -8,6 +8,28 @@ public class vacantUrinalsCount {
         if(vacant_urinal){
             return -1;
         }
-     return 0;
+        int initialCount = (int) str.chars().filter(ch -> ch == '1').count();
+
+        int[] Array = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            Array[i] = Character.getNumericValue(str.charAt(i));
+        }
+
+        for (int i = 0; i < Array.length; i++) {
+            int urinal = Array[i];
+            if (i == 0) {
+                if (Array.length == 1) {
+                    return 1 - Integer.parseInt(String.valueOf(urinal));
+                } else if ((urinal == 0) && Array[i + 1] == 0) {
+                    Array[i] = 1;
+                }
+            } else if (i == Array.length - 1) {
+                if (urinal == 0 && Array[i - 1] == 0) {
+                    Array[i] = 1;
+                }
+            }
+        }
+
+        return (int) Arrays.stream(Array).filter(x -> x == 1).count() - initialCount;
     }
 }
